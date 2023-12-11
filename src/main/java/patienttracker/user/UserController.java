b880e8ca6ed95c945648d83ae75c3cd1ce398a9a
Ishan Bhardwaj <ibhardwaj@umass.edu>
2023-12-11 17:05:35 -0500

    Add CRUD operations for users

--- src/main/java/patienttracker/user/UserController.java
+++ src/main/java/patienttracker/user/UserController.java
@@ -1,12 +1,12 @@
 package patienttracker.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -16,9 +16,26 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers() {
         return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public void addNewUser(@RequestBody User user) {
+        this.userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        this.userService.deleteUser(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateUser(@PathVariable("id") Long id,
+                           @RequestParam(required = false) String username,
+                           @RequestParam(required = false) String password) {
+        this.userService.updateUser(id, username, password);
     }
 
 }
--- src/main/java/patienttracker/user/UserService.java
+++ src/main/java/patienttracker/user/UserService.java
@@ -3,9 +3,8 @@ package patienttracker.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,4 +20,37 @@ public class UserService {
         return this.userRepository.findAll();
     }
 
+    public void addNewUser(User user) {
+        Optional<User> userByUsername = this.userRepository.findUserByUsername(user.getUsername());
+        if (userByUsername.isPresent()) {
+            throw new IllegalStateException("Username already exists");
+        }
+        this.userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        if (!this.userRepository.existsById(id)) {
+            throw new IllegalStateException("The user with ID " + id + " does not exist");
+        }
+        this.userRepository.deleteById(id);
+    }
+
+    public void updateUser(Long id, String username, String password) {
+        User user = this.userRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("The user with ID " + id + " does not exist"));
+
+        if (username != null && !username.isEmpty() && !username.equals(user.getUsername())) {
+            Optional<User> userByUsername = this.userRepository.findUserByUsername(username);
+            if (userByUsername.isPresent()) {
+                throw new IllegalStateException("Username already exists");
+            }
+            user.setUsername(username);
+        }
+
+        if (password != null && !password.isEmpty() && !password.equals(user.getPassword())) {
+            user.setPassword(password);
+        }
+
+        this.userRepository.save(user);
+    }
 }
